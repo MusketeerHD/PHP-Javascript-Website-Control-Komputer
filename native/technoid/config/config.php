807ce7fef6f882808b9320efeb654fca0a492bc3
MusketeerHD <121343844+MusketeerHD@users.noreply.github.com>
2022-12-26 02:09:47 +0700

    Add files via upload

---
+++ native/technoid/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*----- Kofigurasi Database -----*/
+define('hostname', '127.0.0.1');
+define('username', 'root');
+define('password', '');
+define('database', 'tekno');
+
+/*----- Url -----*/
+define('url', 'http://localhost/native/technoid/');
+
+/*----- Author -----*/
+define('author', 'ControlKomp.');
